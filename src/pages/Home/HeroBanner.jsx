1d38b1fa4ecd5af7df5d770f088399ab248ddd71
sachin-dashzer <sachin8287037611@gmail.com>
2024-11-03 23:41:27 +0530

    banner function completed

--- src/pages/Home/HeroBanner.jsx
+++ src/pages/Home/HeroBanner.jsx
@@ -1,66 +1,102 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import carImg from '../../assets/c1.png'
 import carImg2 from '../../assets/c2.png'
-// import carImg3 from '../../assets/c3.png'
-import carImg4 from '../../assets/c4.png'
+import carImg3 from '../../assets/c4.png'
 
 const HeroBanner = () => {
 
 
     const [carActive, setcarActive] = useState(0)
-    const [carColor, setcarColor] = useState('red')
+    // const [carColor, setcarColor] = useState('red')
 
+    const cars = [
+        {
+            name: 'car1',
+            img: carImg,
+            color: '#8a251e'
+        },
+        {
+            name: 'car2',
+            img: carImg2,
+            color: '#2c2c2c'
+        },
+        {
+            name: 'car3',
+            img: carImg3,
+            color: '#5eae38'
+        }
+    ];
+
+
+
+    
 
     const nextCar = () => {
-        if (carActive == 2) {
+        if (carActive >= 2) {
             setcarActive(0)
         } else {
-            setcarActive(carActive + 1)
+            setcarActive(() => carActive + 1)
         }
+
+
     }
+    
+
+    const prevCar = () => {
+        if (carActive <= 0) {
+            setcarActive(() => cars.length - 1)
+        } else {
+            setcarActive(() => carActive - 1)
+        }
+        
+        
+
+    }
+
 
 
     return (
         <>
 
 
-            <section className="heroBanner h-[700px] w-full pt-20" style={{ backgroundColor: `${carColor}` }}>
+            <section className="heroBanner h-[700px] w-full pt-20 transition-all duration-[1s] ease-in-out " style={{ backgroundColor: `${cars[carActive].color}` }}>
 
                 <div className="bannerContainer px-16 pt-20 relative">
 
                     <div className="bannerTexts relative h-16 overflow-hidden">
-                        <div className={`bannertextBox absolute left-12 transition-all duration-[1s] ease-in-out ${carActive == 0 ? 'top-0 opacity-100' : 'top-full opacity-0'}`}>
-                            <h3 className="text-5xl font-thin">Car-1</h3>
-                        </div>
-                        <div className={`bannertextBox absolute left-12 transition-all duration-[1s] ease-in-out ${carActive == 1 ? 'top-0 opacity-100' : 'top-full opacity-0'}`}>
-                            <h3 className="text-5xl font-thin">Car-2</h3>
-                        </div>
-                        <div className={`bannertextBox absolute left-12 transition-all duration-[1s] ease-in-out ${carActive == 2 ? 'top-0 opacity-100' : 'top-full opacity-0'}`}>
-                            <h3 className="text-5xl font-thin">Car-3</h3>
-                        </div>
+
+                        {
+                            cars.map((car, index) => {
+                                return (
+                                    <div key={index} className={`bannertextBox absolute left-12 transition-all duration-[1s] ease-in-out ${carActive == index ? 'top-0 opacity-100' : 'top-full opacity-0'}`}>
+                                        <h3 className="text-5xl font-thin">{car.name}</h3>
+                                    </div>
+                                )
+                            })
+                        }
+
                     </div>
 
                     <div className="bannerImages absolute top-20 left-0 w-full">
 
                         <div className="bannerImgContainer relative w-full h-[450px] flex justify-center">
 
-                            <div className={`BannerImg w-3/5 mx-auto h-[450px] absolute transition-all duration-[1s] ease-in-out  ${carActive == 0 ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}>
-                                <img src={carImg} alt="" />
-                            </div>
-                            <div className={`BannerImg w-3/5 mx-auto h-[450px] absolute transition-all duration-[1s] ease-in-out ${carActive == 1 ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}>
-                                <img src={carImg2} alt="" />
+                            {
+                                cars.map((car, index) => {
+                                    return (
+                                        <div key={index} className={`BannerImg w-3/5 mx-auto h-[450px] absolute transition-all duration-[1s] ease-in-out ${carActive == index ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}>
+                                            <img src={car.img} alt="" />
+                                        </div>
+                                    )
+                                })
+                            }
 
-                            </div>
 
-                            <div className={`BannerImg w-3/5 mx-auto h-[450px] absolute transition-all duration-[1s] ease-in-out ${carActive == 2 ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}>
-                                <img src={carImg4} alt="" />
-
-                            </div>
-                            <div className="dropBtns absolute">
+                            <div className="dropBtns absolute z-40">
                                 <div className="dropBtnBox relative w-full h-full">
-                                    <button className=" absolute bottom-[-30px] left-[55%] w-16 h-16 bg-slate-100"></button>
-                                    <button className="absolute bottom-[-27px] right-[30%] w-16 h-16 bg-slate-100"></button>
+                                    <button onClick={prevCar} className=" absolute bottom-[-25px] left-[58%] z-50 bg-white text-black cursor-pointer rounded-full w-14 h-14 text-xl"><i className="fa-solid fa-angle-left"></i></button>
+                                    <button onClick={nextCar} className="absolute bottom-[-21px] right-[30%] z-50 bg-white text-black cursor-pointer rounded-full w-14 h-14 text-xl"><i className="fa-solid fa-angle-right"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -80,7 +116,6 @@ const HeroBanner = () => {
 
 
 
-                <button onClick={nextCar}>Next</button>
 
             </section>
 
